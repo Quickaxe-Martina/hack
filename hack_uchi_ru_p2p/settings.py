b570513c47e2705fbac6a626bc76f09e532d3842
19311223 <SIgorBogdanov@sberbank.ru>
2021-10-01 03:28:40 +0300

    add postman

--- hack_uchi_ru_p2p/settings.py
+++ hack_uchi_ru_p2p/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'hack_uchi_ru_p2p.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('POSTGRES_DB', 'selectel'),
-#         'USER': os.getenv('POSTGRES_USER', 'selectel'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'selectel'),
-#         'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-#         'PORT': int(os.getenv('POSTGRES_PORT', '1414')),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'selectel'),
+        'USER': os.getenv('POSTGRES_USER', 'selectel'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'selectel'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': int(os.getenv('POSTGRES_PORT', '5432')),
+    }
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
