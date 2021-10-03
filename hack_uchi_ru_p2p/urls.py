70b0895f7d4aadfda7abbea0961dd8b68ec0fb8f
19311223 <SIgorBogdanov@sberbank.ru>
2021-10-03 08:40:04 +0300

    fix

--- hack_uchi_ru_p2p/urls.py
+++ hack_uchi_ru_p2p/urls.py
@@ -24,48 +24,48 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # user
-    path('user/register/', RegisterAPIView.as_view()),
-    path('user/auth/', UserAuthAPIView.as_view()),
-    path('user/load_avatar/', UserAvatarAPIView.as_view()),
-    path('user/<int:pk>/', UserDetailView.as_view()),
-    path('user/', UserListView.as_view()),
-    path('user_dashboard/', UserDashboardAPIView.as_view()),
-    path('user_achievement/', UserAchievementAPIView.as_view()),
+    path('api/user/register/', RegisterAPIView.as_view()),
+    path('api/user/auth/', UserAuthAPIView.as_view()),
+    path('api/user/load_avatar/', UserAvatarAPIView.as_view()),
+    path('api/user/<int:pk>/', UserDetailView.as_view()),
+    path('api/user/', UserListView.as_view()),
+    path('api/user_dashboard/', UserDashboardAPIView.as_view()),
+    path('api/user_achievement/', UserAchievementAPIView.as_view()),
 
     # faculty
-    path('faculty/<int:pk>/', FacultyRetrieveView.as_view()),
-    path('faculty/', FacultyListView.as_view()),
+    path('api/faculty/<int:pk>/', FacultyRetrieveView.as_view()),
+    path('api/faculty/', FacultyListView.as_view()),
 
     # subject
-    path('subject/', SubjectListView.as_view()),
+    path('api/subject/', SubjectListView.as_view()),
 
     # topic
-    path('topic/', TopicListView.as_view()),
-    path('best_topic/', BestTopicAPIView.as_view()),
+    path('api/topic/', TopicListView.as_view()),
+    path('api/best_topic/', BestTopicAPIView.as_view()),
 
     # question
-    path('question/<int:pk>/', QuestionDetailAPIView.as_view()),
-    path('question/', QuestionListView.as_view()),
-    path('best_question/', BestQuestionsAPIView.as_view()),
+    path('api/question/<int:pk>/', QuestionDetailAPIView.as_view()),
+    path('api/question/', QuestionListView.as_view()),
+    path('api/best_question/', BestQuestionsAPIView.as_view()),
 
     # answer
-    path('answer/<int:pk>/', AnswerDetailAPIView.as_view()),
-    path('answer/', AnswerListView.as_view()),
+    path('api/answer/<int:pk>/', AnswerDetailAPIView.as_view()),
+    path('api/answer/', AnswerListView.as_view()),
 
     # like
-    path('like/', LikeAPIView.as_view()),
+    path('api/like/', LikeAPIView.as_view()),
 
     # chat
-    path('chat/', ChatAPIView.as_view()),
-    path('message/', MessageChatListCreateView.as_view()),
+    path('api/chat/', ChatAPIView.as_view()),
+    path('api/message/', MessageChatListCreateView.as_view()),
 
     # shop
-    path('shop_items/', ShopItemListView.as_view()),
-    path('purchases/', PurchasesListView.as_view()),
+    path('api/shop_items/', ShopItemListView.as_view()),
+    path('api/purchases/', PurchasesListView.as_view()),
 
     # achievement
-    path('achievement/<int:pk>/', AchievementRetrieveView.as_view()),
-    path('achievement/', AchievementListView.as_view()),
+    path('api/achievement/<int:pk>/', AchievementRetrieveView.as_view()),
+    path('api/achievement/', AchievementListView.as_view()),
 
 ]
 
